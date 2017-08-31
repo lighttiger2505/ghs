92c4adcc9e963cc45b9d703558dc11feddc9f56a
lighttiger <lighttiger2505@gmail.com>
2017-08-31 22:16:30 +0900

    Add code subcommand of option flags

--- main.go
+++ main.go
@@ -132,10 +132,80 @@ var commandCommit = cli.Command{
 }
 
 var commandCode = cli.Command{
-	Name:    "code",
-	Aliases: []string{"c"},
-	Usage:   "Search codes",
-	Action:  doCode,
+	Name:      "code",
+	UsageText: "ghs code [command options] [query]",
+	Aliases:   []string{"c"},
+	Usage:     "Search codes",
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "in",
+			Usage: "Qualifies which fields are searched. With this qualifier you can restrict the search to the file contents (file), the file path (path), or both.",
+		},
+		cli.StringFlag{
+			Name:  "language",
+			Usage: "Searches code based on the language it's written in.",
+		},
+		cli.StringFlag{
+			Name:  "fork",
+			Usage: "Specifies that code from forked repositories should be searched (true). Repository forks will not be searchable unless the fork has more stars than the parent repository.",
+		},
+		cli.StringFlag{
+			Name:  "size",
+			Usage: "Finds files that match a certain size (in bytes).",
+		},
+		cli.StringFlag{
+			Name:  "path",
+			Usage: "Specifies the path prefix that the resulting file must be under.",
+		},
+		cli.StringFlag{
+			Name:  "filename",
+			Usage: "Matches files by a substring of the filename.",
+		},
+		cli.StringFlag{
+			Name:  "extension",
+			Usage: "Matches files with a certain extension after a dot.",
+		},
+		cli.StringFlag{
+			Name:  "user",
+			Usage: "Limits searches to a specific user.",
+		},
+		cli.StringFlag{
+			Name:  "repo",
+			Usage: "Limits searches to a specific repository.",
+		},
+		cli.StringFlag{
+			Name:  "sort, s",
+			Usage: "The sort field. One of stars, forks, or updated. Default: results are sorted by best match.",
+		},
+		cli.StringFlag{
+			Name:  "order, o",
+			Value: "desc",
+			Usage: "The sort order if sort parameter is provided. One of asc or desc.",
+		},
+		cli.IntFlag{
+			Name:  "num, n",
+			Value: 10,
+			Usage: "Display num of search results.",
+		},
+		cli.IntFlag{
+			Name:  "page, p",
+			Value: 1,
+			Usage: "Display page of search results.",
+		},
+		cli.BoolFlag{
+			Name:  "only",
+			Usage: "Draw repository full name only",
+		},
+		cli.BoolFlag{
+			Name:  "oneline",
+			Usage: "Draw repository online",
+		},
+		cli.BoolFlag{
+			Name:  "table",
+			Usage: "Draw table",
+		},
+	},
+	Action: doCode,
 }
 
 var commandIssue = cli.Command{
